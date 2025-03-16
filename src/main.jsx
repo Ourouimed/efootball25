3bb6823a4b1e557fcf4faf7f1d3b9e755e656cfc
medtube835 <medtube835@gmail.com>
2025-03-16 10:55:17 +0000

    fix router

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './assets/fonts/fonts.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
