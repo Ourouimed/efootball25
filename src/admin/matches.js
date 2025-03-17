9eba539c6178aeb25ea538612b101045ad912549
medtube835 <medtube835@gmail.com>
2025-03-17 21:58:19 +0000

    Create new draw

--- src/admin/matches.js
+++ src/admin/matches.js
@@ -1,28 +1,115 @@
 const matches = [
     [
       {
-        "teamA": "L9irch",
-        "teamB": "Maroc",
-        "score": "Vs"
-      },
-      {
-        "teamA": "T9aba",
-        "teamB": "Zlala",
-        "score": "Vs"
-      },
-      {
         "teamA": "5yearsjail",
-        "teamB": "Bayern munchin",
-        "score": "Vs"
-      },
-      {
-        "teamA": "Inter milan",
         "teamB": "FAR",
         "score": "Vs"
       },
       {
-        "teamA": "Aquile06",
+        "teamA": "Zlala",
         "teamB": "Real betis",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Real betis",
+        "teamB": "T9aba",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Real betis",
+        "teamB": "Raja atheltic club",
+        "score": "Vs"
+      },
+      {
+        "teamA": "BARÇA",
+        "teamB": "5yearsjail",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Real betis",
+        "teamB": "Everton",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Madrid",
+        "teamB": "jamaica 🇯🇲",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Inter milan",
+        "teamB": "Deateam",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Tauhid",
+        "teamB": "L9irch",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Deateam",
+        "teamB": "العداب اخويا العداب",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Ac milan",
+        "teamB": "Real betis",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Aquile06",
+        "teamB": "Inter milan",
+        "score": "Vs"
+      }
+    ],
+    [
+      {
+        "teamA": "jamaica 🇯🇲",
+        "teamB": "FAR",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Deateam",
+        "teamB": "Ac milan",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Raja atheltic club",
+        "teamB": "العداب اخويا العداب",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Everton",
+        "teamB": "Inter milan",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Ac milan",
+        "teamB": "العداب اخويا العداب",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Real betis",
+        "teamB": "Deateam",
+        "score": "Vs"
+      },
+      {
+        "teamA": "5yearsjail",
+        "teamB": "Inter milan",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Ross county FC",
+        "teamB": "Deateam",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Aquile06",
+        "teamB": "Maroc",
+        "score": "Vs"
+      },
+      {
+        "teamA": "L9irch",
+        "teamB": "Madrid",
         "score": "Vs"
       },
       {
@@ -31,164 +118,40 @@ const matches = [
         "score": "Vs"
       },
       {
-        "teamA": "العداب اخويا العداب",
+        "teamA": "BARÇA",
         "teamB": "Ac milan",
-        "score": "Vs"
-      },
-      {
-        "teamA": "F PES ANA SULTAN",
-        "teamB": "jamaica 🇯🇲",
-        "score": "Vs"
-      },
-      {
-        "teamA": "Wydad Ac",
-        "teamB": "Deateam",
-        "score": "Vs"
-      },
-      {
-        "teamA": "Nottingham Forest",
-        "teamB": "Madrid",
-        "score": "Vs"
-      },
-      {
-        "teamA": "sn3",
-        "teamB": "Raja atheltic club",
-        "score": "Vs"
-      },
-      {
-        "teamA": "Tauhid",
-        "teamB": "Ross county FC",
         "score": "Vs"
       }
     ],
     [
       {
-        "teamA": "Tauhid",
-        "teamB": "العداب اخويا العداب",
-        "score": "Vs"
-      },
-      {
-        "teamA": "Bayern munchin",
+        "teamA": "Nottingham Forest",
         "teamB": "5yearsjail",
         "score": "Vs"
       },
       {
-        "teamA": "Real betis",
-        "teamB": "FAR",
+        "teamA": "L9irch",
+        "teamB": "Everton",
         "score": "Vs"
       },
       {
-        "teamA": "Maroc",
-        "teamB": "F PES ANA SULTAN",
+        "teamA": "Real betis",
+        "teamB": "Ross county FC",
         "score": "Vs"
       },
       {
         "teamA": "Ac milan",
-        "teamB": "L9irch",
-        "score": "Vs"
-      },
-      {
-        "teamA": "Deateam",
-        "teamB": "Everton",
-        "score": "Vs"
-      },
-      {
-        "teamA": "Madrid",
-        "teamB": "T9aba",
-        "score": "Vs"
-      },
-      {
-        "teamA": "BARÇA",
-        "teamB": "Nottingham Forest",
-        "score": "Vs"
-      },
-      {
-        "teamA": "Wydad Ac",
-        "teamB": "jamaica 🇯🇲",
-        "score": "Vs"
-      },
-      {
-        "teamA": "Zlala",
-        "teamB": "Aquile06",
-        "score": "Vs"
-      },
-      {
-        "teamA": "Raja atheltic club",
-        "teamB": "Inter milan",
-        "score": "Vs"
-      },
-      {
-        "teamA": "sn3",
-        "teamB": "Ross county FC",
-        "score": "Vs"
-      }
-    ],
-    [
-      {
-        "teamA": "5yearsjail",
-        "teamB": "Bayern munchin",
-        "score": "Vs"
-      },
-      {
-        "teamA": "F PES ANA SULTAN",
-        "teamB": "L9irch",
-        "score": "Vs"
-      },
-      {
-        "teamA": "Maroc",
-        "teamB": "Raja atheltic club",
-        "score": "Vs"
-      },
-      {
-        "teamA": "العداب اخويا العداب",
-        "teamB": "T9aba",
-        "score": "Vs"
-      },
-      {
-        "teamA": "Real betis",
-        "teamB": "Everton",
-        "score": "Vs"
-      },
-      {
-        "teamA": "jamaica 🇯🇲",
-        "teamB": "sn3",
-        "score": "Vs"
-      },
-      {
-        "teamA": "Inter milan",
         "teamB": "Madrid",
         "score": "Vs"
       },
       {
         "teamA": "BARÇA",
-        "teamB": "Wydad Ac",
-        "score": "Vs"
-      },
-      {
-        "teamA": "Ross county FC",
-        "teamB": "Aquile06",
-        "score": "Vs"
-      },
-      {
-        "teamA": "Tauhid",
-        "teamB": "Nottingham Forest",
+        "teamB": "sn3",
         "score": "Vs"
       },
       {
         "teamA": "Zlala",
-        "teamB": "FAR",
-        "score": "Vs"
-      },
-      {
-        "teamA": "Ac milan",
-        "teamB": "Deateam",
-        "score": "Vs"
-      }
-    ],
-    [
-      {
-        "teamA": "T9aba",
-        "teamB": "Tauhid",
+        "teamB": "Raja atheltic club",
         "score": "Vs"
       },
       {
@@ -197,301 +160,338 @@ const matches = [
         "score": "Vs"
       },
       {
-        "teamA": "Bayern munchin",
-        "teamB": "Aquile06",
-        "score": "Vs"
-      },
-      {
-        "teamA": "jamaica 🇯🇲",
-        "teamB": "F PES ANA SULTAN",
-        "score": "Vs"
-      },
-      {
-        "teamA": "Everton",
-        "teamB": "العداب اخويا العداب",
-        "score": "Vs"
-      },
-      {
-        "teamA": "Real betis",
-        "teamB": "sn3",
-        "score": "Vs"
-      },
-      {
-        "teamA": "Madrid",
-        "teamB": "FAR",
-        "score": "Vs"
-      },
-      {
-        "teamA": "Ac milan",
-        "teamB": "BARÇA",
-        "score": "Vs"
-      },
-      {
-        "teamA": "5yearsjail",
-        "teamB": "Ross county FC",
-        "score": "Vs"
-      },
-      {
-        "teamA": "Wydad Ac",
-        "teamB": "Inter milan",
-        "score": "Vs"
-      },
-      {
-        "teamA": "Zlala",
-        "teamB": "Maroc",
-        "score": "Vs"
-      },
-      {
-        "teamA": "L9irch",
-        "teamB": "Deateam",
-        "score": "Vs"
-      }
-    ],
-    [
-      {
-        "teamA": "Wydad Ac",
-        "teamB": "Ross county FC",
-        "score": "Vs"
-      },
-      {
-        "teamA": "العداب اخويا العداب",
-        "teamB": "F PES ANA SULTAN",
-        "score": "Vs"
-      },
-      {
-        "teamA": "Bayern munchin",
-        "teamB": "Maroc",
-        "score": "Vs"
-      },
-      {
-        "teamA": "jamaica 🇯🇲",
-        "teamB": "Everton",
-        "score": "Vs"
-      },
-      {
-        "teamA": "BARÇA",
-        "teamB": "5yearsjail",
-        "score": "Vs"
-      },
-      {
-        "teamA": "Ac milan",
-        "teamB": "Inter milan",
-        "score": "Vs"
-      },
-      {
-        "teamA": "T9aba",
-        "teamB": "Aquile06",
-        "score": "Vs"
-      },
-      {
-        "teamA": "Zlala",
-        "teamB": "Real betis",
-        "score": "Vs"
-      },
-      {
-        "teamA": "Nottingham Forest",
-        "teamB": "FAR",
-        "score": "Vs"
-      },
-      {
-        "teamA": "Deateam",
-        "teamB": "sn3",
-        "score": "Vs"
-      },
-      {
-        "teamA": "Madrid",
-        "teamB": "Tauhid",
-        "score": "Vs"
-      },
-      {
-        "teamA": "L9irch",
-        "teamB": "Raja atheltic club",
-        "score": "Vs"
-      }
-    ],
-    [
-      {
-        "teamA": "BARÇA",
-        "teamB": "Tauhid",
-        "score": "Vs"
-      },
-      {
-        "teamA": "Deateam",
-        "teamB": "Real betis",
-        "score": "Vs"
-      },
-      {
-        "teamA": "jamaica 🇯🇲",
-        "teamB": "Raja atheltic club",
-        "score": "Vs"
-      },
-      {
-        "teamA": "العداب اخويا العداب",
+        "teamA": "Inter milan",
         "teamB": "Madrid",
         "score": "Vs"
       },
       {
         "teamA": "Zlala",
-        "teamB": "Ross county FC",
+        "teamB": "Madrid",
         "score": "Vs"
       },
-      {
-        "teamA": "Aquile06",
-        "teamB": "F PES ANA SULTAN",
-        "score": "Vs"
-      },
-      {
-        "teamA": "Nottingham Forest",
-        "teamB": "FAR",
-        "score": "Vs"
-      },
-      {
-        "teamA": "sn3",
-        "teamB": "Everton",
-        "score": "Vs"
-      },
-      {
-        "teamA": "Bayern munchin",
-        "teamB": "L9irch",
-        "score": "Vs"
-      },
-      {
-        "teamA": "T9aba",
-        "teamB": "5yearsjail",
-        "score": "Vs"
-      },
-      {
-        "teamA": "Wydad Ac",
-        "teamB": "Maroc",
-        "score": "Vs"
-      },
-      {
-        "teamA": "Inter milan",
-        "teamB": "Ac milan",
-        "score": "Vs"
-      }
-    ],
-    [
       {
         "teamA": "Maroc",
-        "teamB": "Deateam",
-        "score": "Vs"
-      },
-      {
-        "teamA": "F PES ANA SULTAN",
-        "teamB": "L9irch",
-        "score": "Vs"
-      },
-      {
-        "teamA": "Bayern munchin",
-        "teamB": "Nottingham Forest",
-        "score": "Vs"
-      },
-      {
-        "teamA": "5yearsjail",
-        "teamB": "jamaica 🇯🇲",
-        "score": "Vs"
-      },
-      {
-        "teamA": "BARÇA",
-        "teamB": "sn3",
-        "score": "Vs"
-      },
-      {
-        "teamA": "العداب اخويا العداب",
-        "teamB": "Wydad Ac",
-        "score": "Vs"
-      },
-      {
-        "teamA": "Madrid",
-        "teamB": "FAR",
-        "score": "Vs"
-      },
-      {
-        "teamA": "Aquile06",
         "teamB": "Tauhid",
         "score": "Vs"
       },
       {
-        "teamA": "Everton",
-        "teamB": "Inter milan",
+        "teamA": "sn3",
+        "teamB": "Real betis",
         "score": "Vs"
       },
       {
-        "teamA": "T9aba",
-        "teamB": "Ac milan",
-        "score": "Vs"
-      },
-      {
-        "teamA": "Ross county FC",
+        "teamA": "Bayern munchin",
         "teamB": "Zlala",
         "score": "Vs"
-      },
-      {
-        "teamA": "Raja atheltic club",
-        "teamB": "Real betis",
-        "score": "Vs"
       }
     ],
     [
       {
-        "teamA": "Wydad Ac",
-        "teamB": "العداب اخويا العداب",
+        "teamA": "Madrid",
+        "teamB": "Wydad Ac",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Inter milan",
+        "teamB": "Ac milan",
         "score": "Vs"
       },
       {
         "teamA": "BARÇA",
-        "teamB": "Tauhid",
+        "teamB": "F PES ANA SULTAN",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Nottingham Forest",
+        "teamB": "sn3",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Everton",
+        "teamB": "Nottingham Forest",
         "score": "Vs"
       },
       {
         "teamA": "F PES ANA SULTAN",
+        "teamB": "Wydad Ac",
+        "score": "Vs"
+      },
+      {
+        "teamA": "T9aba",
+        "teamB": "Nottingham Forest",
+        "score": "Vs"
+      },
+      {
+        "teamA": "BARÇA",
         "teamB": "Inter milan",
-        "score": "Vs"
-      },
-      {
-        "teamA": "Ross county FC",
-        "teamB": "jamaica 🇯🇲",
-        "score": "Vs"
-      },
-      {
-        "teamA": "sn3",
-        "teamB": "Real betis",
-        "score": "Vs"
-      },
-      {
-        "teamA": "Bayern munchin",
-        "teamB": "Everton",
         "score": "Vs"
       },
       {
         "teamA": "FAR",
-        "teamB": "Maroc",
+        "teamB": "Bayern munchin",
         "score": "Vs"
       },
       {
-        "teamA": "Madrid",
-        "teamB": "Nottingham Forest",
+        "teamA": "sn3",
+        "teamB": "Madrid",
         "score": "Vs"
       },
       {
-        "teamA": "Deateam",
-        "teamB": "L9irch",
-        "score": "Vs"
-      },
-      {
-        "teamA": "Ac milan",
-        "teamB": "Aquile06",
+        "teamA": "FAR",
+        "teamB": "Tauhid",
         "score": "Vs"
       },
       {
         "teamA": "5yearsjail",
-        "teamB": "Zlala",
+        "teamB": "Ac milan",
+        "score": "Vs"
+      }
+    ],
+    [
+      {
+        "teamA": "T9aba",
+        "teamB": "Raja atheltic club",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Ac milan",
+        "teamB": "Nottingham Forest",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Ross county FC",
+        "teamB": "Everton",
+        "score": "Vs"
+      },
+      {
+        "teamA": "sn3",
+        "teamB": "Wydad Ac",
+        "score": "Vs"
+      },
+      {
+        "teamA": "L9irch",
+        "teamB": "Real betis",
+        "score": "Vs"
+      },
+      {
+        "teamA": "العداب اخويا العداب",
+        "teamB": "Inter milan",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Everton",
+        "teamB": "Madrid",
         "score": "Vs"
       },
       {
         "teamA": "Raja atheltic club",
+        "teamB": "BARÇA",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Nottingham Forest",
+        "teamB": "Zlala",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Ross county FC",
+        "teamB": "Madrid",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Ross county FC",
+        "teamB": "Zlala",
+        "score": "Vs"
+      },
+      {
+        "teamA": "العداب اخويا العداب",
+        "teamB": "Nottingham Forest",
+        "score": "Vs"
+      }
+    ],
+    [
+      {
+        "teamA": "العداب اخويا العداب",
+        "teamB": "BARÇA",
+        "score": "Vs"
+      },
+      {
+        "teamA": "5yearsjail",
+        "teamB": "jamaica 🇯🇲",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Ross county FC",
         "teamB": "T9aba",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Aquile06",
+        "teamB": "Ac milan",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Wydad Ac",
+        "teamB": "Ac milan",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Aquile06",
+        "teamB": "Zlala",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Wydad Ac",
+        "teamB": "Ross county FC",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Maroc",
+        "teamB": "5yearsjail",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Bayern munchin",
+        "teamB": "Madrid",
+        "score": "Vs"
+      },
+      {
+        "teamA": "F PES ANA SULTAN",
+        "teamB": "5yearsjail",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Maroc",
+        "teamB": "Nottingham Forest",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Tauhid",
+        "teamB": "Aquile06",
+        "score": "Vs"
+      }
+    ],
+    [
+      {
+        "teamA": "Real betis",
+        "teamB": "Inter milan",
+        "score": "Vs"
+      },
+      {
+        "teamA": "sn3",
+        "teamB": "Deateam",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Nottingham Forest",
+        "teamB": "Bayern munchin",
+        "score": "Vs"
+      },
+      {
+        "teamA": "T9aba",
+        "teamB": "العداب اخويا العداب",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Deateam",
+        "teamB": "Maroc",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Maroc",
+        "teamB": "العداب اخويا العداب",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Real betis",
+        "teamB": "Tauhid",
+        "score": "Vs"
+      },
+      {
+        "teamA": "5yearsjail",
+        "teamB": "العداب اخويا العداب",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Zlala",
+        "teamB": "L9irch",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Aquile06",
+        "teamB": "F PES ANA SULTAN",
+        "score": "Vs"
+      },
+      {
+        "teamA": "jamaica 🇯🇲",
+        "teamB": "Deateam",
+        "score": "Vs"
+      },
+      {
+        "teamA": "L9irch",
+        "teamB": "Nottingham Forest",
+        "score": "Vs"
+      }
+    ],
+    [
+      {
+        "teamA": "Bayern munchin",
+        "teamB": "T9aba",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Wydad Ac",
+        "teamB": "FAR",
+        "score": "Vs"
+      },
+      {
+        "teamA": "L9irch",
+        "teamB": "F PES ANA SULTAN",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Inter milan",
+        "teamB": "Raja atheltic club",
+        "score": "Vs"
+      },
+      {
+        "teamA": "sn3",
+        "teamB": "T9aba",
+        "score": "Vs"
+      },
+      {
+        "teamA": "BARÇA",
+        "teamB": "Bayern munchin",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Everton",
+        "teamB": "Aquile06",
+        "score": "Vs"
+      },
+      {
+        "teamA": "F PES ANA SULTAN",
+        "teamB": "Raja atheltic club",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Bayern munchin",
+        "teamB": "العداب اخويا العداب",
+        "score": "Vs"
+      },
+      {
+        "teamA": "5yearsjail",
+        "teamB": "Deateam",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Zlala",
+        "teamB": "5yearsjail",
+        "score": "Vs"
+      },
+      {
+        "teamA": "Bayern munchin",
+        "teamB": "jamaica 🇯🇲",
         "score": "Vs"
       }
     ]
