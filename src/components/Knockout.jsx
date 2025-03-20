a72ce22e2e65ef3d2d618eb6daff23aff640b558
medtube835 <medtube835@gmail.com>
2025-03-20 11:37:04 +0000

    Create Knoukout

--- src/components/Knockout.jsx
+++ src/components/Knockout.jsx
@@ -13,7 +13,7 @@ const Knockout = () => {
         <div className="p-2 mb-1">
             <h3 className="text-center font-bold">Playoffs</h3>
             <div className="grid grid-cols-[1fr_1fr] md:flex items-center gap-1 justify-center">
-                    {PO.length > 0 ? PO.map((match , index) => <div key={index} className="match">
+                    {PO.length > 0 ? PO.map((match , index) => <div key={index} className="match !mb-1">
                                 <span className='flex-1 text-center'>{match.teamA}</span>
                                 <span className={`mx-4 font-bold py-1 px-4 rounded ${match.score !== 'Vs'  && match.score !== 'PP' ? 'bg-fourth text-white' : null}`}>{match.score}</span>
                                 <span className='flex-1 text-center'>{match.teamB}</span>
@@ -21,7 +21,7 @@ const Knockout = () => {
             </div>
             <h3 className="text-center font-bold">Quarter Final</h3>
             <div className="grid grid-cols-[1fr_1fr] md:flex items-center gap-1 justify-center">
-                    {QF.length > 0 ? QF.map((match , index) => <div key={index} className="match">
+                    {QF.length > 0 ? QF.map((match , index) => <div key={index} className="match !mb-1">
                                 <span className='flex-1 text-center'>{match.teamA}</span>
                                 <span className={`mx-4 font-bold py-1 px-4 rounded ${match.score !== 'Vs'  && match.score !== 'PP' ? 'bg-fourth text-white' : null}`}>{match.score}</span>
                                 <span className='flex-1 text-center'>{match.teamB}</span>
@@ -30,7 +30,7 @@ const Knockout = () => {
 
             <h3 className="text-center font-bold">Semi Final</h3>
             <div className="grid grid-cols-[1fr_1fr] md:flex items-center gap-1 justify-center">
-                    {SF.length > 0 ? SF.map((match , index) => <div key={index} className="match">
+                    {SF.length > 0 ? SF.map((match , index) => <div key={index} className="match !mb-1">
                                 <span className='flex-1 text-center'>{match.teamA}</span>
                                 <span className={`mx-4 font-bold py-1 px-4 rounded ${match.score !== 'Vs'  && match.score !== 'PP' ? 'bg-fourth text-white' : null}`}>{match.score}</span>
                                 <span className='flex-1 text-center'>{match.teamB}</span>
@@ -38,7 +38,7 @@ const Knockout = () => {
             </div>
             <h3 className="text-center font-bold">Final</h3>
             <div className="flex items-center gap-1 justify-center">
-                <div className="match">
+                <div className="match !mb-0">
                                     <span className='flex-1 text-center'>{Final.teamA}</span>
                                     <span className={`mx-4 font-bold py-1 px-4 rounded ${Final.score !== 'Vs'  && Final.score !== 'PP' ? 'bg-fourth text-white' : null}`}>{Final.score}</span>
                                     <span className='flex-1 text-center'>{Final.teamB}</span>
