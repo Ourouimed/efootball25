b4c9d661d543d5013bbe764100bf9d201779fa6e
medtube835 <medtube835@gmail.com>
2025-03-21 00:07:36 +0000

    resonsive

--- src/components/Knockout.jsx
+++ src/components/Knockout.jsx
@@ -12,7 +12,7 @@ const Knockout = () => {
         <h1 className="font-bold text-xl text-center bg-fourth p-2 text-white">Knoukout</h1>
         <div className="p-2 mb-1">
             <h3 className="text-center font-bold">Playoffs</h3>
-            <div className="grid grid-cols-[1fr_1fr] md:flex items-center gap-1 justify-center">
+            <div className="grid grid-cols-[1fr] md:flex md:items-center gap-1 md:justify-center">
                     {PO.length > 0 ? PO.map((match , index) => <div key={index} className="match !mb-1">
                                 <span className='flex-1 text-center'>{match.teamA}</span>
                                 <span className={`mx-4 font-bold py-1 px-4 rounded ${match.score !== 'Vs'  && match.score !== 'PP' ? 'bg-fourth text-white' : null}`}>{match.score}</span>
@@ -20,7 +20,7 @@ const Knockout = () => {
                 </div> ) : <h1 className='text-center text-xl'>No Availible matches</h1>}
             </div>
             <h3 className="text-center font-bold">Quarter Final</h3>
-            <div className="grid grid-cols-[1fr_1fr] md:flex items-center gap-1 justify-center">
+            <div className="grid grid-cols-[1fr] md:flex md:items-center gap-1 md:justify-center">
                     {QF.length > 0 ? QF.map((match , index) => <div key={index} className="match !mb-1">
                                 <span className='flex-1 text-center'>{match.teamA}</span>
                                 <span className={`mx-4 font-bold py-1 px-4 rounded ${match.score !== 'Vs'  && match.score !== 'PP' ? 'bg-fourth text-white' : null}`}>{match.score}</span>
@@ -29,7 +29,7 @@ const Knockout = () => {
             </div>
 
             <h3 className="text-center font-bold">Semi Final</h3>
-            <div className="grid grid-cols-[1fr_1fr] md:flex items-center gap-1 justify-center">
+            <div className="grid grid-cols-[1fr] md:flex md:items-center gap-1 md:justify-center">
                     {SF.length > 0 ? SF.map((match , index) => <div key={index} className="match !mb-1">
                                 <span className='flex-1 text-center'>{match.teamA}</span>
                                 <span className={`mx-4 font-bold py-1 px-4 rounded ${match.score !== 'Vs'  && match.score !== 'PP' ? 'bg-fourth text-white' : null}`}>{match.score}</span>
