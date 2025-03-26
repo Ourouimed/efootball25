7079337dee3ac483b51e37a4dafb852db699a8eb
medtube835 <medtube835@gmail.com>
2025-03-26 22:35:41 +0000

    set teams

--- src/admin/teams.js
+++ src/admin/teams.js
@@ -1,243 +1,233 @@
 const teams = [
     {
-        teamName: 'Zlala',
-        teamId: 'Tammarxa',
-        GF: 22,
-        GA: 10,
-        wins: 5,
-        draws: 1,
-        losses: 2,
-        KG: 10
-    },
-    {
-        teamName: 'Madrid',
-        teamId: 'anaspop',
-        GF: 15,
-        GA: 11,
-        wins: 4,
-        draws: 1,
-        losses: 3,
-        KG: 0
-    },
-    {
-        teamName: 'F PES ANA SULTAN',
-        teamId: 'KAIDO_MORRI',
-        GF: 19,
-        GA: 15,
-        wins: 4,
-        draws: 1,
-        losses: 3,
-        KG: 3
-    },
-    {
-        teamName: 'Ross county FC',
-        teamId: 'Hichamn Jerando',
-        GF: 23,
-        GA: 10,
-        wins: 6,
-        draws: 0,
-        losses: 2,
-        KG: 2
-    },
-    {
-        teamName: 'Nottingham Forest',
-        teamId: 'ernesto_lll',
-        GF: 14,
-        GA: 23,
-        wins: 3,
-        draws: 0,
-        losses: 5,
-        KG: 0
-    },
-    {
-        teamName: 'sn3',
-        teamId: 'sn3',
-        GF: 5,
-        GA: 25,
+        teamName: 'FC BARCELONA',
+        teamId: 'RAPHAA',
+        GF: 0,
+        GA: 0,
         wins: 0,
-        draws: 1,
-        losses: 7,
-        KG: 0
-    },
-    {
-        teamName: 'Deateam',
-        teamId: 'asr_dealoo',
-        GF: 14,
-        GA: 39,
-        wins: 1,
         draws: 0,
-        losses: 7,
-        KG: 0
-    },
-    {
-        teamName: 'Ac milan',
-        teamId: 'Ostora9',
-        GF: 16,
-        GA: 36,
-        wins: 3,
-        draws: 0,
-        losses: 5,
-        KG: 0
-    },
-    {
-        teamName: 'BARÇA',
-        teamId: 'azzedine',
-        GF: 4,
-        GA: 20,
-        wins: 0,
-        draws: 2,
-        losses: 6,
-        KG: 7
-    },
-    {
-        teamName: 'stuntman',
-        teamId: 'Reda',
-        GF: 25,
-        GA: 12,
-        wins: 6,
-        draws: 0,
-        losses: 2,
-        KG: 4
-    },
-    {
-        teamName: 'Raja atheltic club',
-        teamId: 'lmtiri',
-        GF: 6,
-        GA: 27,
-        wins: 0,
-        draws: 1,
-        losses: 7,
-        KG: 0
-    },
-    {
-        teamName: 'Inter milan',
-        teamId: 'Laamirchi_05',
-        GF: 17,
-        GA: 16,
-        wins: 2,
-        draws: 3,
-        losses: 3,
-        KG: 0
-    },
-    {
-        teamName: 'jamaica 🇯🇲',
-        teamId: 'to9os_ifri9ya',
-        GF: 23,
-        GA: 9,
-        wins: 7,
-        draws: 0,
-        losses: 1,
-        KG: 12
-    },
-    {
-        teamName: 'Maroc',
-        teamId: 'hamzaelalmi',
-        GF: 17,
-        GA: 7,
-        wins: 6,
-        draws: 0,
-        losses: 2,
-        KG: 15
-    },
-    {
-        teamName: 'الحصان الاسود',
-        teamId: 'NO9TA',
-        GF: 30,
-        GA: 23,
-        wins: 4,
-        draws: 0,
-        losses: 4,
-        KG: 0
-    },
-    {
-        teamName: 'L9irch',
-        teamId: '_moncef_',
-        GF: 34,
-        GA: 8,
-        wins: 6,
-        draws: 2,
         losses: 0,
-        KG: 7
-    },
-    {
-        teamName: 'Bayern munchin',
-        teamId: 'Svgasfuck',
-        GF: 28,
-        GA: 12,
-        wins: 7,
-        draws: 0,
-        losses: 1,
-        KG: 10
-    },
-    {
-        teamName: 'FAR',
-        teamId: 'Tareeq_uar',
-        GF: 4,
-        GA: 22,
-        wins: 0,
-        draws: 2,
-        losses: 6,
-        KG: 0
-    },
-    {
-        teamName: '5yearsjail',
-        teamId: '5yearsjail',
-        GF: 2,
-        GA: 21,
-        wins: 0,
-        draws: 2,
-        losses: 6,
-        KG: 0
-    },
-    {
-        teamName: 'العداب اخويا العداب',
-        teamId: 'Mehdi-Rhou',
-        GF: 14,
-        GA: 9,
-        wins: 5,
-        draws: 1,
-        losses: 2,
-        KG: 4
-    },
-    {
-        teamName: 'Wydad Ac',
-        teamId: 'Amine_L7ya',
-        GF: 20,
-        GA: 16,
-        wins: 4,
-        draws: 1,
-        losses: 3,
-        KG: 4
-    },
-    {
-        teamName: 'T9aba',
-        teamId: 'Fost3er',
-        GF: 25,
-        GA: 12,
-        wins: 6,
-        draws: 0,
-        losses: 2,
-        KG: 11
+        KOGF : 0 ,
+        KOGA : 0
     },
     {
         teamName: 'Aquile06',
         teamId: 'Aquile06',
-        GF: 31,
-        GA: 14,
-        wins: 5,
-        draws: 1,
-        losses: 2,
-        KG: 12
+        GF: 0,
+        GA: 0,
+        wins: 0,
+        draws: 0,
+        losses: 0,
+        KOGF : 0 ,
+        KOGA : 0
     },
     {
-        teamName: 'Real betis',
-        teamId: 'العريق بيتيس',
-        GF: 10,
-        GA: 19,
-        wins: 2,
-        draws: 1,
-        losses: 5,
-        KG: 0
+        teamName: 'Raja atheltic club',
+        teamId: 'Lamtiri',
+        GF: 0,
+        GA: 0,
+        wins: 0,
+        draws: 0,
+        losses: 0,
+        KOGF : 0 ,
+        KOGA : 0
+    },
+    {
+        teamName: 'Ross country FC',
+        teamId: 'Hicham jerando',
+        GF: 0,
+        GA: 0,
+        wins: 0,
+        draws: 0,
+        losses: 0,
+        KOGF : 0 ,
+        KOGA : 0
+    }
+    ,
+    {
+        teamName: 'F PES ANA SULTAN',
+        teamId: 'KAIDO_MORRI',
+        GF: 0,
+        GA: 0,
+        wins: 0,
+        draws: 0,
+        losses: 0,
+        KOGF : 0 ,
+        KOGA : 0
+    }
+    ,
+    {
+        teamName: 'المدمر 33',
+        teamId: 'NO9TA',
+        GF: 0,
+        GA: 0,
+        wins: 0,
+        draws: 0,
+        losses: 0,
+        KOGF : 0 ,
+        KOGA : 0
+    }
+    ,
+    {
+        teamName: '....',
+        teamId: 'Mamsalich',
+        GF: 0,
+        GA: 0,
+        wins: 0,
+        draws: 0,
+        losses: 0,
+        KOGF : 0 ,
+        KOGA : 0
+    }
+    ,
+    {
+        teamName: 'Ac milan',
+        teamId: '3mek_d',
+        GF: 0,
+        GA: 0,
+        wins: 0,
+        draws: 0,
+        losses: 0,
+        KOGF : 0 ,
+        KOGA : 0
+    }
+    ,
+    {
+        teamName: 'Ro3b🫣',
+        teamId: 'To9os ifri9ya',
+        GF: 0,
+        GA: 0,
+        wins: 0,
+        draws: 0,
+        losses: 0,
+        KOGF : 0 ,
+        KOGA : 0
+    }
+    ,
+    {
+        teamName: 'العذاب اخويا العذاب',
+        teamId: 'Mehdi-rhou',
+        GF: 0,
+        GA: 0,
+        wins: 0,
+        draws: 0,
+        losses: 0,
+        KOGF : 0 ,
+        KOGA : 0
+    }
+    ,
+    {
+        teamName: 'Itihad zaghalghal',
+        teamId: 'Aymen oudich',
+        GF: 0,
+        GA: 0,
+        wins: 0,
+        draws: 0,
+        losses: 0,
+        KOGF : 0 ,
+        KOGA : 0
+    }
+    ,
+    {
+        teamName: 'L9irch',
+        teamId: '_monceff__',
+        GF: 0,
+        GA: 0,
+        wins: 0,
+        draws: 0,
+        losses: 0,
+        KOGF : 0 ,
+        KOGA : 0
+    }
+    ,
+    {
+        teamName: 'zlala',
+        teamId: 'Tammar',
+        GF: 0,
+        GA: 0,
+        wins: 0,
+        draws: 0,
+        losses: 0,
+        KOGF : 0 ,
+        KOGA : 0
+    },
+    {
+        teamName: 'Maroc',
+        teamId: 'Hamza El almi',
+        GF: 0,
+        GA: 0,
+        wins: 0,
+        draws: 0,
+        losses: 0,
+        KOGF : 0 ,
+        KOGA : 0
+    },
+    {
+        teamName: 'LOS REYES1!',
+        teamId: 'Wassiim_the_reaaal',
+        GF: 0,
+        GA: 0,
+        wins: 0,
+        draws: 0,
+        losses: 0,
+        KOGF : 0 ,
+        KOGA : 0
+    } ,
+    {
+        teamName: 'Madrid',
+        teamId: 'pop',
+        GF: 0,
+        GA: 0,
+        wins: 0,
+        draws: 0,
+        losses: 0,
+        KOGF : 0 ,
+        KOGA : 0
+    },
+    {
+        teamName: 'T9aba',
+        teamId: 'Fost3er',
+        GF: 0,
+        GA: 0,
+        wins: 0,
+        draws: 0,
+        losses: 0,
+        KOGF : 0 ,
+        KOGA : 0
+    },
+    {
+        teamName: 'Bayern Munich',
+        teamId: 'SVG AS FUCK',
+        GF: 0,
+        GA: 0,
+        wins: 0,
+        draws: 0,
+        losses: 0,
+        KOGF : 0 ,
+        KOGA : 0
+    },
+    {
+        teamName: 'Stutman',
+        teamId: 'Reda',
+        GF: 0,
+        GA: 0,
+        wins: 0,
+        draws: 0,
+        losses: 0,
+        KOGF : 0 ,
+        KOGA : 0
+    }
+    ,
+    {
+        teamName: 'River Plate',
+        teamId: 'Ostora9',
+        GF: 0,
+        GA: 0,
+        wins: 0,
+        draws: 0,
+        losses: 0,
+        KOGF : 0 ,
+        KOGA : 0
     }
 ];
 
