03a3ca95d4d7d1ad0634fb1b62d207cffd82288e
medtube835 <medtube835@gmail.com>
2025-05-06 16:44:04 +0000

    Create PO draw

--- backend/index.js
+++ backend/index.js
@@ -86,7 +86,7 @@ function generatePoMatches(teams){
   let matches = []
   let sortedTeams = teams
   .map(team => ({...team , pts : (Number(team.wins) * 3) + (Number(team.draws) * 1) + (Number(team.losses) * 0)}))
-  .sort((a, b) => b.pts - a.pts || (b.GF - b.GA) - (a.GF - a.GA))
+  .sort((a, b) => b.pts - a.pts || (b.GF - b.GA) - (a.GF - a.GA)).slice(8 , 24)
 
   for (let i = 0; i < sortedTeams.length / 2 ;i++){
     let homeTeamIndex= Math.floor(Math.random() * sortedTeams.length)
